f1a7f160656741db382c6427015b507b29fffc37
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-10-07 16:10:22 -0600

    feat(domain): repositorio de job que tiene la definicion del metodo para guardar o actualizar una vacane

--- src/job/domain/ports/job_repository_port.py
+++ src/job/domain/ports/job_repository_port.py
@@ -1,6 +1,4 @@
-from typing import TYPE_CHECKING, Protocol, Set
-
-from job.domain.value_objects.job_categories import JobCategory
+from typing import TYPE_CHECKING, Protocol
 
 if TYPE_CHECKING:
     from job.domain.job_entity import Job
@@ -8,9 +6,3 @@ if TYPE_CHECKING:
 
 class JobRepositoryPort(Protocol):
     async def save(self, job: "Job") -> None: ...
-    async def get_by_category(self, category: JobCategory) -> Set["Job"]: ...
-    async def get_unique_categories_by_user(self, user_id: str) -> Set["Job"]:
-        """
-        Obtiene una lista de categorías únicas asociadas a las postulaciones de un usuario.
-        """
-        ...
